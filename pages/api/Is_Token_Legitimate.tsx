45cfd1e587d8fd640d75f1007fa463dfb8ebea88
Mike Ball <michaelball357@gmail.com>
2024-12-20 17:16:25 -0800

    actually good

--- pages/api/Is_Token_Legitimate.tsx
+++ pages/api/Is_Token_Legitimate.tsx
@@ -21,7 +21,32 @@ import verify_id_token_helper from '@/lib/_firebase/server/Verify_Firebase_Auth_
 //require imports are disabled by default, but this is how the documentation does it so I'm not going to do it differently
 /* eslint-disable @typescript-eslint/no-require-imports */
 const admin = require("firebase-admin");
-const serviceAccount = require('@/simple-blog-admin-sdk-key.json');
+
+const serviceAccount = {
+
+  type: process.env.firebase_sdk_type,
+  
+  project_id: process.env.firebase_sdk_project_id,
+  
+  private_key_id: process.env.firebase_sdk_private_key_id,
+  
+  private_key: process.env.firebase_sdk_private_key,
+  
+  client_email: process.env.firebase_sdk_client_email,
+  
+  client_id: process.env.firebase_sdk_client_id,
+  
+  auth_uri: process.env.firebase_sdk_auth_uri,
+  
+  token_uri: process.env.firebase_sdk_token_uri,
+  
+  auth_provider_x509_cert_url: process.env.firebase_sdk_auth_provider_x509_cert_url,
+  
+  client_x509_cert_url: process.env.firebase_sdk_client_x509_cert_url,
+  
+  universe_domain: process.env.firebase_sdk_universe_domain
+  
+  };
 /* eslint-enable @typescript-eslint/no-require-imports */
 
 //make this an asynchronous function because verifying the token iidToken : strings an API call
