563994424201f01580cdc990f4fe4c545371661f
Mike Ball <michaelball357@gmail.com>
2024-12-20 17:23:54 -0800

    modified veryify id token

--- lib/_firebase/server/Verify_Firebase_Auth_Helper.tsx
+++ lib/_firebase/server/Verify_Firebase_Auth_Helper.tsx
@@ -4,7 +4,31 @@
 
 /* eslint-disable @typescript-eslint/no-require-imports */
 const admin = require("firebase-admin");
-const serviceAccount = require('@/simple-blog-admin-sdk-key.json');
+const serviceAccount = {
+
+    type: process.env.firebase_sdk_type,
+    
+    project_id: process.env.firebase_sdk_project_id,
+    
+    private_key_id: process.env.firebase_sdk_private_key_id,
+    
+    private_key: process.env.firebase_sdk_private_key,
+    
+    client_email: process.env.firebase_sdk_client_email,
+    
+    client_id: process.env.firebase_sdk_client_id,
+    
+    auth_uri: process.env.firebase_sdk_auth_uri,
+    
+    token_uri: process.env.firebase_sdk_token_uri,
+    
+    auth_provider_x509_cert_url: process.env.firebase_sdk_auth_provider_x509_cert_url,
+    
+    client_x509_cert_url: process.env.firebase_sdk_client_x509_cert_url,
+    
+    universe_domain: process.env.firebase_sdk_universe_domain
+    
+    };
 /* eslint-enable @typescript-eslint/no-require-imports */
 
 const verify_id_token_helper = async(idToken: string) => {
