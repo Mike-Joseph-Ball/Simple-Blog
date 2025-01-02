8e21b54f183ead1bb693550477befec7c3329f36
root <root@ip-10-0-28-128.ec2.internal>
2025-01-02 04:10:12 +0000

    got rid of json file

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Enables React Strict Mode
+  swcMinify: true,       // Uses SWC for faster builds and smaller bundles
 
-    //ALLOWS FOR asyncWebAssembly
-    webpack(config) {
-        
-        // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-        config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    
-        return config;
-        
-      },
-    
+  // Enable advanced Webpack configurations
+  webpack(config) {
+    // Enable WebAssembly
+    config.experiments = { 
+      ...config.experiments, 
+      asyncWebAssembly: true 
     };
 
+    // Example: Custom rules or plugins can go here if needed
+    // config.plugins.push(new SomeCustomWebpackPlugin());
+
+    return config;
+  },
+};
+
 export default nextConfig;
+
